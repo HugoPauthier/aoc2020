18ded97d69ad64e548b9f43d50edaedc0448e46f
unknown <hugo.pauthier@gmail.com>
2020-12-23 02:07:57 +0100

    Implementing day2

--- Program.cs
+++ Program.cs
@@ -57,12 +57,84 @@ namespace aoc2020
 
         public static void display()
         {
-            Console.WriteLine("******************** DAY 1 ********************");
+            Console.WriteLine("\n******************** DAY 1 ********************");
             Console.WriteLine("Part1 = " + Day1.part1());
             Console.WriteLine("Part2 = " + Day1.part2());
             Console.WriteLine("***********************************************");
         }
 
+    }
+
+    class Day2
+    {
+        public static int part1() 
+        {
+            string filePath = "./input_Day2.txt";
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            int res = 0;
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string line = lines[i];
+
+                char[] separators = new char[] { ' ', '-', ':' };
+                string[] subs = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                int policyMin = int.Parse(subs[0]);
+                int policyMax = int.Parse(subs[1]);
+                char letter = char.Parse(subs[2]);
+                string password = subs[3];
+                // Console.WriteLine($"{i} {policyMin} {policyMax} {letter} {password} ");
+
+                int freq = password.Count(f => (f == letter));
+                if (freq >= policyMin && freq <= policyMax)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+
+        public static int part2()
+        {
+            string filePath = "./input_Day2.txt";
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            int res = 0;
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string line = lines[i];
+
+                char[] separators = new char[] { ' ', '-', ':' };
+                string[] subs = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                int index1 = int.Parse(subs[0]);
+                int index2 = int.Parse(subs[1]);
+                char letter = char.Parse(subs[2]);
+                string password = subs[3];
+                // Console.WriteLine($"{i} {index1} {index2} {letter} {password} ");
+
+                if (password[index1 - 1] == letter && password[index2 - 1] != letter)
+                {
+                    res++;
+                }
+                if (password[index1 - 1] != letter && password[index2 - 1] == letter)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+
+        public static void display()
+        {
+            Console.WriteLine("\n******************** DAY 2 ********************");
+            Console.WriteLine("Part1 = " + Day2.part1());
+            Console.WriteLine("Part2 = " + Day2.part2());
+            Console.WriteLine("***********************************************");
+        }
     }
 
 
@@ -71,6 +143,7 @@ namespace aoc2020
         static void Main(string[] args)
         {
             Day1.display();
+            Day2.display();
         }
     }
 
